2733ae39c48f97f5523bc0bcdebfa82da6e30aa5
MariiaSimakova <mariia.simakova6@gmail.com>
2024-02-09 12:25:30 +1000

    react-router-dom 5.3.0

---
+++ src/components/pages/ComicsPage.js
@@ -0,0 +1,13 @@
+import AppBanner from "../appBanner/AppBanner";
+import ComicList from "../comicsList/ComicsList";
+
+const ComicsPage = () => {
+  return (
+    <>
+      <AppBanner />
+      <ComicList />
+    </>
+  );
+};
+
+export default ComicsPage;
