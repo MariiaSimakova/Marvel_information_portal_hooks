b7147c93ad38ca4877c0e100c8eaef8cbde0d0c4
MariiaSimakova <mariia.simakova6@gmail.com>
2024-02-13 15:01:55 +1000

    added single comic page

--- src/components/comicsList/ComicsList.js
+++ src/components/comicsList/ComicsList.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import useMarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
+import { Link } from "react-router-dom";
 import "./comicsList.scss";
 
 const ComicsList = () => {
@@ -35,7 +36,7 @@ const ComicsList = () => {
     const items = arr.map((item, i) => {
       return (
         <li className="comics__item" key={i}>
-          <a href="#">
+          <Link to={`/comics/${item.id}`}>
             <img
               src={item.thumbnail}
               alt={item.title}
@@ -43,7 +44,7 @@ const ComicsList = () => {
             />
             <div className="comics__item-name">{item.title}</div>
             <div className="comics__item-price">{item.price}</div>
-          </a>
+          </Link>
         </li>
       );
     });
